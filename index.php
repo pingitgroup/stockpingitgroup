9b5ffaa935a1268a31abfd205b863ca711b5abca
pingitgroup <pingitgroup@gmail.com>
2015-08-30 00:53:42 +0700

    saran

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+// path URL in Zend Framework
 	$protocol = ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1')? 'http':'http';
 	header("Location: ".$protocol."://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."public");
 ?>
